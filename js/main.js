afeccce6739322a8e2ee3fc1b46be900a0e0f019
Taylie Whitworth <105079720+tayliewhitworth@users.noreply.github.com>
2023-04-12 15:00:07 -0600

    Added a theme.css

--- js/main.js
+++ js/main.js
@@ -11,6 +11,30 @@ const cartItemContainer = document.querySelector(".cart-items");
 const cartRows = document.querySelectorAll(".cart-row");
 const cartTotal = document.querySelector(".cart-total-price");
 
+// adding theme stylesheet
+document.getElementById('theme').addEventListener('click', function() {
+  toggleStylesheet('css/theme.css')
+})
+
+function toggleStylesheet(href, onoff) {
+  let existingNode = 0 //get existing stylesheet node if it already exists:
+  for (let i = 0; i < document.styleSheets.length; i++) {
+    if (document.styleSheets[i].href && document.styleSheets[i].href.indexOf(href) > -1) existingNode = document.styleSheets[i].ownerNode
+  }
+  if (onoff == undefined) onoff = !existingNode //toggle on or off if undefined
+  if (onoff) { //TURN ON:
+    if (existingNode) return onoff //already exists so cancel now
+    let link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = href;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  } else { //TURN OFF:
+    if (existingNode) existingNode.parentNode.removeChild(existingNode)
+  }
+  return onoff
+}
+
 // show and hide items
 let contentSections = ["home", "about", "tips", "shop"];
 let navElements = document.querySelectorAll(".nav-list li");
